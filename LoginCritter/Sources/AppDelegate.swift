f9fbf34372706dedf58646d016cf5ff78e67606c
Vishal Dharankar <vdharankar@gmail.com>
2020-03-11 18:00:45 +0530

    Initial build
    Initial build

---
+++ LoginCritter/Sources/AppDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.swift
+//  LoginCritter
+//
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setUpWindow()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) { }
+
+    func applicationDidEnterBackground(_ application: UIApplication) { }
+
+    func applicationWillEnterForeground(_ application: UIApplication) { }
+
+    func applicationDidBecomeActive(_ application: UIApplication) { }
+
+    func applicationWillTerminate(_ application: UIApplication) { }
+
+    // MARK: - Private
+
+    private func setUpWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = LoginViewController()
+        window?.makeKeyAndVisible()
+    }
+}
